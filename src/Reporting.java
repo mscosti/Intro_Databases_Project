d70f097129dd73e09cedbb0c4569f06038f5cc1c
mscosti <mscosti@wpi.edu>
2013-12-11 16:33:08 -0500

    Implemented Patient Reporting

--- src/Reporting.java
+++ src/Reporting.java
@@ -3,71 +3,103 @@ import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class Reporting
 {
-		public static void main(String [] args)
-		{
-			if (args.length == 1)
-			{
-				if (!connectToJDBC()) return;
+	private static Scanner scanner = new Scanner(System.in);
+	
 
-				switch (Integer.valueOf(args[0]))
-				{
-					case 1:
-						System.out.println("Reporting Patient");
-						break;
-					case 2:
-						System.out.println("Reporting Doctors");
-						break;
-					case 3:
-						System.out.println("Report Admission Info");
-						break;
-					case 4:
-						System.out.println("Update Payment");
-						break;
-				}
+	public static void main(String [] args) throws SQLException
+	{
+		if (args.length == 1)
+		{
+			Connection conn = connectToJDBC();
+			if (conn == null) return;
+
+			System.out.println(args[0]);
+			switch (Integer.valueOf(args[0]))
+			{
+				case 1:
+
+					System.out.println("Enter Patient SSN: ");
+					String SSN = scanner.nextLine();
+					Statement stmt = conn.createStatement();
+					ResultSet rset = stmt.executeQuery
+						("SELECT SSN, fName, lName, address "+
+						 "FROM Patient WHERE SSN = '" + SSN + "'");
+
+					displayPatient(rset);
+
+					break;
+				case 2:
+					System.out.println("Reporting Doctors");
+					break;
+				case 3:
+					System.out.println("Report Admission Info");
+					break;
+				case 4:
+					System.out.println("Update Payment");
+					break;
 			}
-			else
-				displayPrompt();
+		}
+		else
+			displayPrompt();
+	}
+
+	public static void displayPrompt()
+	{
+		String prompt =  "1- Report Patients Basic Information\n"
+						+"2- Report Doctors Basic Information\n"
+						+"3- Report Admissions Information\n"
+						+"4- Update Admissions Payment\n";
+		System.out.println(prompt);
+	}
+
+	public static void displayPatient(ResultSet patient) throws SQLException
+	{
+		patient.next();
+		String SSN = patient.getString("SSN");
+		String firstName = patient.getString("fName");
+		String lastName = patient.getString("lName");
+		String address = patient.getString("address");
+
+		String output = "Patient SSN: " + SSN + "\n"
+						"Patient First Name: " + firstName + "\n"
+						"Patient Last Name: " + lastName + "\n"
+						"Patient Address: " + address;
+
+		System.out.println(output);
+	}
+
+	public static Connection connectToJDBC()
+	{
+		System.out.println("----Oracle JDBC Connection Testing----");
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e){
+			System.out.println("Where is your oracle JDBC Driver?");
+			e.printStackTrace();
+			return null;
 		}
 
-		public static void displayPrompt()
+		System.out.println("Oracle JDBC Driver Registered!");
+		Connection connection = null;
+
+		try 
 		{
-			String prompt =  "1- Report Patients Basic Information\n"
-							+"2- Report Doctors Basic Information\n"
-							+"3- Report Admissions Information\n"
-							+"4- Update Admissions Payment\n";
-			System.out.println(prompt);
-		}
-
-		public static Connection connectToJDBC()
+			connection = DriverManager.getConnection(
+										"jdbc:oracle:thin:@oracle.wpi.edu:1521:WPI11grxx",
+										"mscosti","MSCOSTI");
+		} catch (SQLException e)
 		{
-			System.out.println("----Oracle JDBC Connection Testing----");
-			try
-			{
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-			} catch (ClassNotFoundException e){
-				System.out.println("Where is your oracle JDBC Driver?");
-				e.printStackTrace();
-				return null;
-			}
-
-			System.out.println("Oracle JDBC Driver Registered!");
-			Connection connection = null;
-
-			try 
-			{
-				connection = DriverManager.getConnection(
-											"jdbc:oracle:thin:@oracle.wpi.edu:1521:WPI11grxx",
-											"mscosti","MSCOSTI");
-			} catch (SQLException e)
-			{
-				System.out.println("connection failed!");
-				e.printStackTrace();
-				return null;
-			}
-			return connection;
+			System.out.println("connection failed!");
+			e.printStackTrace();
+			return null;
 		}
+		return connection;
+	}
 }
 
